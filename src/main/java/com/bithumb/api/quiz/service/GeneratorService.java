e02bfb85afb5dde0133de2445cd1b016500e1f0e
김청화 <asder36@naver.com>
2021-08-27 00:46:27 +0900

    Mockito  사용한 목업 테스트

---
+++ src/main/java/com/bithumb/api/quiz/service/GeneratorService.java
@@ -0,0 +1,6 @@
+package com.bithumb.api.quiz.service;
+
+public interface GeneratorService {
+    int randomFactor();
+
+}
---
+++ src/main/java/com/bithumb/api/quiz/service/QuizService.java
@@ -0,0 +1,7 @@
+package com.bithumb.api.quiz.service;
+
+import com.bithumb.api.quiz.domain.Quiz;
+
+public interface QuizService {
+    Quiz createQuiz();
+}
